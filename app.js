019fa3b91497c0f448fdf1d9976c30eb9b6fb355
DuplaDOG <dupladog0@gmail.com>
2024-04-05 09:01:34 -0300

    back-2

---
+++ app.js
@@ -0,0 +1,8 @@
+// modelo através do método require
+
+// manipular módulo os (operational system)
+
+const os=require('os');
+console.log(os.cpus());
+console.log(os.cpu[0]);
+ 
---
+++ app1.js
@@ -0,0 +1,12 @@
+console.log('Nomes');
+
+//para importar os nomes que estão no arquivo dados.js
+
+let {nomes, frutas, Soma}=require('./dados');
+
+//console.log(pessoas);
+//console.log(pessoas[0]);
+
+console.log(nomes);
+console.log(frutas);
+console.log(Soma(13,7));
